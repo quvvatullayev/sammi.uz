c6c92025fae8ff524b2ad7626c90b2413df45930
ogabek <quvvatullayev@gmail.com>
2023-07-10 21:35:56 +0500

    create video gallery

--- sammi/models.py
+++ sammi/models.py
@@ -47,3 +47,13 @@ class GalleryImg(models.Model):
     def __str__(self):
         return self.gallery.name
 
+class VideoGallery(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    sees = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
