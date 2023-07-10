a71061c649a5fa516bc28a6e4c981407b039960d
ogabek <quvvatullayev@gmail.com>
2023-07-10 21:31:29 +0500

    create news

--- sammi/models.py
+++ sammi/models.py
@@ -6,3 +6,13 @@ class AboutImg(models.Model):
 
     def __str__(self):
         return self.name
+    
+class News(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    sees = models.IntegerField(default=0)
+    img = models.ImageField(upload_to='news_img')
+
+    def __str__(self):
+        return self.name
