913ff4e61cca364f9f0c5eb85ba824bf71149f5e
ogabek <quvvatullayev@gmail.com>
2023-07-10 21:30:06 +0500

    create about img model

--- sammi/models.py
+++ sammi/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class AboutImg(models.Model):
+    img = models.ImageField(upload_to='about_img')
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
