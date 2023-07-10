12e08d7776b0c66d6b7dc5fab09d23996350b232
ogabek <quvvatullayev@gmail.com>
2023-07-10 21:41:38 +0500

    create quiz

--- sammi/models.py
+++ sammi/models.py
@@ -74,4 +74,11 @@ class MainStatistic(models.Model):
     def __str__(self):
         return 'Main Statistic'
     
+class Quiz(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.question
+    
 
