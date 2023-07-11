0a44d1ab4500072444da7afadc4291e795058da1
ogabek <quvvatullayev@gmail.com>
2023-07-11 21:43:09 +0500

    craete news views

--- sammi/urls.py
+++ sammi/urls.py
@@ -14,3 +14,19 @@ urlpatterns = [
     path('aboutimg/update/<int:pk>/', AboutImgUpdateView.as_view()),
     path('aboutimg/delete/<int:pk>/', AboutImgDeleteView.as_view()),
 ]
+
+from .view.news import (
+    NewsCreateView,
+    NewsListView,
+    NewsDetailView,
+    NewsUpdateView,
+    NewsDeleteView,
+)
+
+urlpatterns += [
+    path('news/create/', NewsCreateView.as_view()),
+    path('news/list/', NewsListView.as_view()),
+    path('news/detail/<int:pk>/', NewsDetailView.as_view()),
+    path('news/update/<int:pk>/', NewsUpdateView.as_view()),
+    path('news/delete/<int:pk>/', NewsDeleteView.as_view()),
+]
---
+++ sammi/view/news.py
@@ -0,0 +1,85 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from ..models import (
+    News,
+)
+from ..serializer import (
+    NewsSerializer,
+)
+
+class NewsCreateView(APIView):
+    @swagger_auto_schema(request_body=NewsSerializer)
+    def post(self, request: Request, format=None):
+        serializer = NewsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    'message': 'news created successfully',
+                    'news': serializer.data
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            {
+                'message': 'invalid data',
+                'errors': serializer.errors
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+class NewsListView(APIView):
+    @swagger_auto_schema(
+            responses={
+                200: NewsSerializer(many=True)
+                }
+            )
+    def get(self, request: Request, format=None):
+        news = News.objects.all()
+        serializer = NewsSerializer(news, many=True)
+        return Response(serializer.data)
+    
+class NewsDetailView(APIView):
+    @swagger_auto_schema(
+            responses={
+                200: NewsSerializer()
+                }
+            )
+    def get(self, request: Request, pk, format=None):
+        news = News.objects.get(pk=pk)
+        serializer = NewsSerializer(news)
+        return Response(serializer.data)
+    
+class NewsUpdateView(APIView):
+    @swagger_auto_schema(request_body=NewsSerializer)
+    def put(self, request: Request, pk, format=None):
+        news = News.objects.get(pk=pk)
+        serializer = NewsSerializer(news, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    'message': 'news updated successfully',
+                    'news': serializer.data
+                }
+            )
+        return Response(
+            {
+                'message': 'invalid data',
+                'errors': serializer.errors
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+class NewsDeleteView(APIView):
+    def delete(self, request: Request, pk, format=None):
+        news = News.objects.get(pk=pk)
+        news.delete()
+        return Response(
+            {
+                'message': 'news deleted successfully',
+            }
+        )
