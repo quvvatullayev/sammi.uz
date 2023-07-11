29851a31d8d4c8b6d2b4ff500311447ca00743fb
ogabek <quvvatullayev@gmail.com>
2023-07-11 21:40:13 +0500

    add admin views

--- sammi/admin.py
+++ sammi/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import (
+    AboutImg,
+    News,
+    NewsImg,
+    Video,
+    Gallery,
+    GalleryImg,
+    VideoGallery,
+    Ad,
+    Quiz,
+    UsefulSites,
+    Contact,
+)
 
 # Register your models here.
+admin.site.register(AboutImg)
+admin.site.register(News)
+admin.site.register(NewsImg)
+admin.site.register(Video)
+admin.site.register(Gallery)
+admin.site.register(GalleryImg)
+admin.site.register(VideoGallery)
+admin.site.register(Ad)
+admin.site.register(Quiz)
+admin.site.register(UsefulSites)
+admin.site.register(Contact)
+
