e2e2721d519555dd7c7721617976824b6e9ba54e
ogabek <quvvatullayev@gmail.com>
2023-07-11 21:38:30 +0500

    fix doc

--- sammi/view/aboutimg.py
+++ sammi/view/aboutimg.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
 from ..models import (
     AboutImg,
 )
@@ -10,6 +11,7 @@ from ..serializer import (
 )
 
 class AboutImgCreateView(APIView):
+    @swagger_auto_schema(request_body=AboutImgSerializer)
     def post(self, request: Request, format=None):
         serializer = AboutImgSerializer(data=request.data)
         if serializer.is_valid():
@@ -24,18 +26,29 @@ class AboutImgCreateView(APIView):
         )
     
 class AboutImgListView(APIView):
+    @swagger_auto_schema(
+            responses={
+                200: AboutImgSerializer(many=True)
+                }
+            )
     def get(self, request: Request, format=None):
         aboutimgs = AboutImg.objects.all()
         serializer = AboutImgSerializer(aboutimgs, many=True)
         return Response(serializer.data)
 
 class AboutImgDetailView(APIView):
+    @swagger_auto_schema(
+            responses={
+                200: AboutImgSerializer()
+                }
+            )
     def get(self, request: Request, pk, format=None):
         aboutimg = AboutImg.objects.get(pk=pk)
         serializer = AboutImgSerializer(aboutimg)
         return Response(serializer.data)
     
 class AboutImgUpdateView(APIView):
+    @swagger_auto_schema(request_body=AboutImgSerializer)
     def put(self, request: Request, pk, format=None):
         aboutimg = AboutImg.objects.get(pk=pk)
         serializer = AboutImgSerializer(aboutimg, data=request.data)
@@ -51,6 +64,11 @@ class AboutImgUpdateView(APIView):
         )
     
 class AboutImgDeleteView(APIView):
+    @swagger_auto_schema(
+            responses={
+                204: 'No Content'
+                }
+            )
     def delete(self, request: Request, pk, format=None):
         aboutimg = AboutImg.objects.get(pk=pk)
         aboutimg.delete()
