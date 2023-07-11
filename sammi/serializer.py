8dbd7e3d68d86a96ed087a37a49b915809576b04
ogabek <quvvatullayev@gmail.com>
2023-07-11 20:02:48 +0500

    create video serialzer

--- sammi/serializer.py
+++ sammi/serializer.py
@@ -29,3 +29,8 @@ class NewsImgSerializer(serializers.ModelSerializer):
         model = NewsImg
         fields = '__all__'
 
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = '__all__'
+
