81c86e43443110689e615095ec10c72b9d869d5f
ogabek <quvvatullayev@gmail.com>
2023-07-12 21:09:31 +0500

    add newsimg urls

--- sammi/urls.py
+++ sammi/urls.py
@@ -30,3 +30,19 @@ urlpatterns += [
     path('news/update/<int:pk>/', NewsUpdateView.as_view()),
     path('news/delete/<int:pk>/', NewsDeleteView.as_view()),
 ]
+
+from .view.newsimg import (
+    NewsImgCreateView,
+    NewsImgListView,
+    NewsImgDetailView,
+    NewsImgUpdateView,
+    NewsImgDeleteView,
+)
+
+urlpatterns += [
+    path('newsimg/create/', NewsImgCreateView.as_view()),
+    path('newsimg/list/', NewsImgListView.as_view()),
+    path('newsimg/detail/<int:pk>/', NewsImgDetailView.as_view()),
+    path('newsimg/update/<int:pk>/', NewsImgUpdateView.as_view()),
+    path('newsimg/delete/<int:pk>/', NewsImgDeleteView.as_view()),
+]
