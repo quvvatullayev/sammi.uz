82ca9153fcbecc872aad47664cd6d4250859edd6
ogabek <quvvatullayev@gmail.com>
2023-07-16 20:32:21 +0500

    create django user serializer

--- sammi/serializers.py
+++ sammi/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import (
     AboutImg,
     News,
@@ -72,6 +73,11 @@ class UsefulSitesSerializer(serializers.ModelSerializer):
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
 
 
